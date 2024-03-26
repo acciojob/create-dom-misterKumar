30321934970cbb64039ca96775cf6001932f4dfb
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-26 08:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function createDom(root) {
 
+	if(typeof root === 'string')
+	{
+		return document.createTextNode(root);
+	}
+
+	const element = document.createElement(root.type);
+
+	if(root.attributes && typeof root.attributes === 'object')
+	{
+		for(const [attrName, attrValue] of Object.entries(root.attributes))
+		{
+			element.setAttribute(attrName, attrValue);
+		}
+	}
+
+	if(typeof root === 'string')
+	{
+		element.textContent = root;
+	}
+
+	if(root.children && Array.isArray(root.children))
+	{
+		for(const child of root.children)
+			{
+				const childNode = createDom(child);
+				element.appendChild(childNode);
+			}
+	}
+	return element;
 }
 
 module.exports=createDom
